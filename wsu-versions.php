1ce1270010bc3b1676b96ee75bb18b476ce2b9af
jeremyfelt <jeremy.felt@gmail.com>
2014-02-20 12:48:30 -0800

    Add plugin header and licence files

---
+++ wsu-versions.php
@@ -0,0 +1,30 @@
+<?php
+/**
+Plugin Name: WSU Versions
+Description: A method of versioning content, location, and form in WordPress.
+Author:      washingtonstateuniversity, jeremyfelt
+Version:     0.1
+Plugin URI:  http://github.com/washingtonstateuniversity/wsuwp-plugin-wsu-versions/
+License:     GPLv2 or Later
+ */
+
+/* WSU Versions
+ *
+ * A method of versioning content, location, and form in WordPress
+ *
+ * Copyright (C) 2014 Washington State University
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
