52b5fa33a5388b3f03c069bcd79ed51cad6bf818
jeremyfelt <jeremy.felt@gmail.com>
2014-02-20 13:49:41 -0800

    Add initial meta box to display WSU Versions information

--- wsu-versions.php
+++ wsu-versions.php
@@ -46,6 +46,30 @@ class WSU_Versions {
 	 */
 	public function __construct() {
 		add_action( 'transition_post_status', array( $this, 'transition_post_status' ), 10, 3 );
+		add_action( 'add_meta_boxes',         array( $this, 'add_meta_boxes'         ), 10, 2 );
+	}
+
+	/**
+	 * Add meta boxes provided by the plugin.
+	 *
+	 * @param string  $post_type Current post's post type.
+	 * @param WP_post $post      Contains properties of a piece of content.
+	 */
+	public function add_meta_boxes( $post_type, $post ) {
+		add_meta_box( 'wsu-versions-meta', 'Versions and Form', array( $this, 'display_versions_box' ), null, 'side', 'default' );
+	}
+
+	/**
+	 * Display a meta box containing WSU Versions information.
+	 *
+	 * @param WP_Post $post Current post's object=.
+	 */
+	public function display_versions_box( $post ) {
+		$unique_id = $this->get_unique_id( $post );
+		$template  = $this->get_template(  $post );
+
+		echo 'Unique ID: <input readonly type="text" value="' . esc_attr( $unique_id ) . '" />';
+		echo 'Template: ' . esc_html( $template );
 	}
 
 	/**
@@ -100,6 +124,21 @@ class WSU_Versions {
 
 		update_post_meta( $post->ID, $this->unique_id_meta_key, $unique_id    );
 		update_post_meta( $post->ID, $this->template_meta_key,  $current_form );
+	}
+
+	/**
+	 * @param int|WP_Post $post Post ID or object representing a piece of content.
+	 *
+	 * @return string|bool String representing template assigned to content. False if not found.
+	 */
+	public function get_template( $post ) {
+		if ( is_object( $post ) ) {
+			$post = $post->ID;
+		}
+
+		$current_form = get_post_meta( $post, $this->template_meta_key, true );
+
+		return $current_form;
 	}
 
 }
