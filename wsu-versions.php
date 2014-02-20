41177decd26c4fd5d4e5791a5bd3ff5464560b17
jeremyfelt <jeremy.felt@gmail.com>
2014-02-20 13:22:08 -0800

    Add initial WSU Versions class with functionality for assigning unique ID
    On post status transition, we determine if a piece of content has a unique ID already. If it does not have a unique ID, then we generate an md5 hash of the post slug, the time, and the post ID.

--- wsu-versions.php
+++ wsu-versions.php
@@ -28,3 +28,67 @@ License:     GPLv2 or Later
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
+
+class WSU_Versions {
+
+	/**
+	 * @var string Stores the meta key used for a piece of content's unique ID.
+	 */
+	var $unique_id_meta_key = '_wsu_versions_uid';
+
+	/**
+	 * Setup the hooks used by the plugin.
+	 */
+	public function __construct() {
+		add_action( 'transition_post_status', array( $this, 'transition_post_status' ), 10, 3 );
+	}
+
+	/**
+	 * Fires when updating or saving a post.
+	 */
+	public function transition_post_status( $new_status, $old_status, $post ) {
+		if ( 'inherit' === $post->post_status ) {
+			return;
+		}
+
+		if ( $this->get_unique_id( $post ) ) {
+			return;
+		}
+
+		$this->generate_unique_id( $post );
+	}
+
+	/**
+	 * Get the unique ID generated for a piece of content.
+	 *
+	 * @param int|object $post Post ID or post object.
+	 *
+	 * @return bool|string False if no unique ID is available or a string representing the unique ID.
+	 */
+	public function get_unique_id( $post ) {
+		if ( is_object( $post ) ) {
+			$post = $post->ID;
+		}
+
+		$unique_id = get_post_meta( $post, $this->unique_id_meta_key, true );
+
+		return $unique_id;
+	}
+
+	/**
+	 * Generate a unique version ID for a piece of content.
+	 *
+	 * @param int|object $post Post ID or post object.
+	 */
+	public function generate_unique_id( $post ) {
+		if ( ! is_object( $post ) ) {
+			$post = get_post( $post );
+		}
+
+		$unique_id = md5( $post->post_name . time() . $post->ID );
+
+		update_post_meta( $post->ID, $this->unique_id_meta_key, $unique_id );
+	}
+
+}
+$wsu_versions = new WSU_Versions();
